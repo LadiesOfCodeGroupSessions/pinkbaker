c0fc45e8cfddd39f9312fd985dbdb3b8dea0d257
Elisa Evans <elisa.evans@outlook.com>
2022-09-07 18:49:56 +0100

    Cakes function

--- baker.py
+++ baker.py
@@ -1,2 +1,4 @@
-def bake():
+def cakes():
+    recipe = {flour: 500, sugar: 200, eggs: 1}
+    available_ingredients = {}
     return 2
--- test_baker.py
+++ test_baker.py
@@ -1,5 +1,5 @@
-from baker import bake
+from baker import cakes
 
-def test_bake():
-    output = bake()
-    assert output == 1
+def test_cakes():
+    output = cakes({'flour': 500, 'sugar': 200, 'eggs': 1}, {'flour': 1200, 'sugar': 1200, 'eggs': 5, 'milk': 200})
+    assert output == 2
