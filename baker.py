e8bcbf1aaf96ce5bc8c2f2e474088fb6966e5aa9
Sarah Usher <sarahnusher@gmail.com>
2022-10-05 19:00:00 +0100

    test case 1 working

--- baker.py
+++ baker.py
@@ -2,6 +2,16 @@ def cakes(recipe, ingredients):
     flour_difference = ingredients['flour']//recipe['flour']
     sugar_difference = ingredients['sugar']//recipe['sugar']
     eggs_difference = ingredients['eggs']//recipe['eggs']
-        print(recipe['flour'])
+
+    arr = [flour_difference, sugar_difference, eggs_difference]
+
+    smallest = 100
+
+    for number in arr:
+        if number < smallest:
+            smallest = number
+
+    return smallest
+
 
 #cakes({'flour': 500, 'sugar': 200, 'eggs': 1}, {'flour': 1200, 'sugar': 1200, 'eggs': 5, 'milk': 200})
