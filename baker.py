89a300ecbc851b0827b02581d029ecdd28f8fe40
clarice_r <claricerdallorto@gmail.com>
2022-10-05 18:38:38 +0100

    Set up first test

--- baker.py
+++ baker.py
@@ -1,3 +1,5 @@
 def cakes(recipe, ingredients):
-    if ingredients > recipe:
-        return 'You can make a cake'
+    for value, key in recipe:
+        print(value, key)
+
+#cakes({'flour': 500, 'sugar': 200, 'eggs': 1}, {'flour': 1200, 'sugar': 1200, 'eggs': 5, 'milk': 200})
--- test_baker.py
+++ test_baker.py
@@ -1,6 +1,6 @@
 from baker import cakes
 
 def test_cakes():
-    #output = cakes({'flour': 500, 'sugar': 200, 'eggs': 1}, {'flour': 1200, 'sugar': 1200, 'eggs': 5, 'milk': 200})
-    output = cakes ( 500, 1200)
-    assert output == 'You can make a cake'
+    output = cakes({'flour': 500, 'sugar': 200, 'eggs': 1},
+                   {'flour': 1200, 'sugar': 1200, 'eggs': 5, 'milk': 200})
+    assert output == 2
