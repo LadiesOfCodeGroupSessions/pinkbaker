aaefc444db79f222ac7afae8276689efd41e62ae
Elisa Evans <elisa.evans@outlook.com>
2022-10-05 19:32:13 +0100

    Tests passed - refactoring 'smallest'

--- baker.py
+++ baker.py
@@ -1,5 +1,5 @@
 def cakes(recipe, ingredients):
-    smallest = 1000
+    smallest = ingredients.items()[0].value #figure this out next time!
     for recipe_key, recipe_value in recipe.items():
         ingredients_value = ingredients.get(recipe_key)
         if ingredients_value is None:
@@ -10,4 +10,4 @@ def cakes(recipe, ingredients):
 
     return smallest
 
-# cakes({'flour': 500, 'sugar': 200, 'eggs': 1}, {'flour': 1200, 'sugar': 1200, 'eggs': 5, 'milk': 200})
+
--- test_baker.py
+++ test_baker.py
@@ -16,3 +16,13 @@ def test_cakes_3():
     output = cakes({'flour': 300, 'sugar': 150, 'milk': 100},
                    {'sugar': 500, 'flour': 2000, 'milk': 2000})
     assert output == 3
+
+def test_cakes_4():
+    output = cakes({'flour': 300, 'sugar': 150, 'milk': 100},
+                   {'sugar': 100, 'flour': 100, 'milk': 50})
+    assert output == 0
+
+def test_cakes_5():
+    output = cakes({'flour': 300, 'sugar': 150, 'milk': 100},
+                   {})
+    assert output == 0
