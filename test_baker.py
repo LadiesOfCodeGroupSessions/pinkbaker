5e5308c697c8e5a7fc1224e88b8e34f3304f83b9
Sarah Usher <sarahnusher@gmail.com>
2022-10-05 19:07:51 +0100

    start of case 3

--- test_baker.py
+++ test_baker.py
@@ -11,3 +11,5 @@ def test_cakes_2():
     output = cakes({'apples': 3, 'flour': 300, 'sugar': 150, 'milk': 100, 'oil': 100},
                    {'sugar': 500, 'flour': 2000, 'milk': 2000})
     assert output == 0
+
+def test_cakes_3():
