4f39598241c347bc321aa8901bef5b5a04176803
Elisa Evans <elisa.evans@outlook.com>
2022-10-05 18:52:58 +0100

    Ingredients vs Recipe variables

--- baker.py
+++ baker.py
@@ -1,5 +1,7 @@
 def cakes(recipe, ingredients):
-    for value, key in recipe:
-        print(value, key)
+    flour_difference = ingredients['flour']//recipe['flour']
+    sugar_difference = ingredients['sugar']//recipe['sugar']
+    eggs_difference = ingredients['eggs']//recipe['eggs']
+        print(recipe['flour'])
 
 #cakes({'flour': 500, 'sugar': 200, 'eggs': 1}, {'flour': 1200, 'sugar': 1200, 'eggs': 5, 'milk': 200})
